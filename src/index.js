8629aa996caba06b27c1fafcc0474006905a655c
petery123 <139976625+petery123@users.noreply.github.com>
2024-09-10 16:25:52 -0400

    Implement page switching

--- src/index.js
+++ src/index.js
@@ -2,5 +2,30 @@ import home from "./home.js"
 import menu from "./menu.js"
 import about from "./about.js"
 
-const pageContent = document.querySelector("#content");
-pageContent.appendChild(about);
+(function(){
+    const pageContent = document.querySelector("#content");
+    pageContent.appendChild(home);
+
+    const navBar = document.querySelector("nav");
+    navBar.addEventListener("click", handleClick);
+
+    function handleClick(event){
+        pageContent.removeChild(pageContent.firstElementChild);
+        const newContent = event.target.id;
+        switch (newContent) {
+            case "home":
+                pageContent.appendChild(home);
+                break;
+            case "menu":
+                pageContent.appendChild(menu);
+                break;
+            case "about":
+                pageContent.appendChild(about);
+                break;
+        }
+    }
+    
+    
+
+    
+})();
